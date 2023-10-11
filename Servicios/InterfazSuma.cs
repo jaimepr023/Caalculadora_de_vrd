fd8bcce5195448c2709a4e6bed2d977a9c310105
DESKTOP-9R3V8HE\profesor <csi23-jpribio@colegioaltair.net>
2023-10-11 11:27:49 +0200

    comentarios

--- Servicios/InterfazSuma.cs
+++ Servicios/InterfazSuma.cs
@@ -8,35 +8,41 @@ namespace calculcadora.Serviciossuma
 {
     /// <summary>
     /// Interfaz con la cabecera del metodo de operaciones
+    /// @autor jpr-111023
     /// </summary>
     internal interface InterfazSuma 
     {
         /// <summary>
         /// Cabecera que devuelve el numero para las operaciones
+        /// @autor jpr-111023
         /// </summary>
         /// <returns></returns>
         public float devuelveUnEntero();
 
         /// <summary>
         /// Cabecera que devuelve la operacion de la suma
+        /// @autor jpr-111023
         /// </summary>
         /// <returns></returns>
         public float operacionsuma(float num1, float num2);
 
         /// <summary>
         /// Cabecera que devuelve la operacion de la resta
+        /// @autor jpr-111023
         /// </summary>
         /// <returns></returns>
         public float operacionresta(float num3, float num4);
 
         /// <summary>
         /// Cabecera que devuelve la operacion de la multiplicacion
+        /// @autor jpr-111023
         /// </summary>
         /// <returns></returns>
         public float opcionmulti(float num5 , float num6);
 
         /// <summary>
         /// Cabecera que devuelve la operacion de la division
+        /// @autor jpr-111023
         /// </summary>
         /// <returns></returns>
         public float opciondivi(float num7, float num8);
